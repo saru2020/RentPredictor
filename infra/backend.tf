17578852d6b880a56877b663f112f0533f1f26ec
Saravanan <saru2020@gmail.com>
2025-07-27 16:00:07 +0530

    ci/cd update: revamped the entire workflow

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "ml-crash-course-terraform-state"
     key    = "infrastructure/terraform.tfstate"
-    region = "us-east-2"  # This will be overridden by -backend-config
+    # region will be set via -backend-config during init
   }
 } 
