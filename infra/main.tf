31fc8840bc01ec836defda2ee8c5a84b9c8b9908
Saravanan <saru2020@gmail.com>
2025-07-27 21:49:26 +0530

    some more ci/cd fixes

--- infra/main.tf
+++ infra/main.tf
@@ -185,3 +185,8 @@ output "eks_node_group_name" {
   description = "Name of the EKS node group"
   value       = aws_eks_node_group.main.node_group_name
 }
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
