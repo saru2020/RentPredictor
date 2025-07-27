b17db9c69af85f6ecae70666ab4ab3356da87bd9
Saravanan <saru2020@gmail.com>
2025-07-27 13:11:19 +0530

    - couple of ci/cd fixes

--- infra/main.tf
+++ infra/main.tf
@@ -23,8 +23,12 @@ module "eks" {
   }
 }
 
-variable "aws_region" {}
-variable "s3_bucket_name" {}
-variable "eks_cluster_name" {}
-variable "eks_subnets" { type = list(string) }
-variable "eks_vpc_id" {}
+output "eks_cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
