47def5635655b73b3959aa079d986b169178140c
Saravanan <saru2020@gmail.com>
2025-07-27 14:18:15 +0530

    some more ci/cd fixes for eks

--- infra/main.tf
+++ infra/main.tf
@@ -40,23 +40,68 @@ resource "aws_s3_bucket" "ml_data" {
   force_destroy = true
 }
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+# Create EKS cluster
+resource "aws_eks_cluster" "main" {
+  name     = var.eks_cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
+  version  = "1.29"
 
-  cluster_name    = var.eks_cluster_name
-  cluster_version = "1.29"
+  vpc_config {
+    subnet_ids              = module.vpc.private_subnets
+    endpoint_public_access  = true
+    security_group_ids      = [aws_security_group.eks_cluster.id]
+  }
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy,
+  ]
+}
 
-  eks_managed_node_groups = {
-    default = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
-      instance_types = ["t3.small"]
-    }
+# Create IAM role for EKS cluster
+resource "aws_iam_role" "eks_cluster" {
+  name = "${var.eks_cluster_name}-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach EKS cluster policy
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+# Create security group for EKS cluster
+resource "aws_security_group" "eks_cluster" {
+  name_prefix = "${var.eks_cluster_name}-cluster-"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.eks_cluster_name}-cluster-sg"
   }
 }
 
